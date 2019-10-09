b670887552bc1c3b8b3893502a670ac2f2de273d
Amit Pandey <amitpandey839@gmail.com>
2019-10-09 22:00:20 +0530

    improve time complexity of problem 220

--- Leetcode/Medium/220. Contains Duplicate III.cpp
+++ Leetcode/Medium/220. Contains Duplicate III.cpp
@@ -1,5 +1,6 @@
 // Link to the problem statement : https://leetcode.com/problems/contains-duplicate-iii/
 
+// Time complexity -> O(n^2)
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
@@ -14,3 +15,46 @@ public:
         return false;
     }
 };
+
+//  Time complexity -> O(n * log(n))
+
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        
+        int n = nums.size();
+    
+        if(n < 2) return false;
+
+        vector<pair<long long,long long>> arr(n);
+
+        for(int i = 0; i < n; i++){
+            arr[i].first = nums[i];
+            arr[i].second = i;
+        }
+        sort(arr.begin(), arr.end());
+
+        int i = 0, j = 0;
+        set<int> st;
+        while(i <= j and j < n){
+            if(arr[j].first - arr[i].first <= t*1L){
+                auto it = st.lower_bound(arr[j].second);
+                if(it != st.end()){
+                    if(abs(*it - arr[j].second) <= k) return true;
+                }
+                if(st.size() > 1 or(st.size() == 1 and it == st.end())){
+                    it--;
+                    if(abs(*it - arr[j].second) <= k) return true;    
+                }
+                st.insert(arr[j].second);
+                j++;
+            }
+            else{
+                st.erase(arr[i].second);
+                i++;
+            }
+        }
+        return false;
+    }
+};
+
