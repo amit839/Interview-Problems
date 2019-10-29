4e329a09637a25a5d748b00c1151cee1c676b04a
Amit Pandey <amitpandey839@gmail.com>
2019-10-29 21:03:38 +0530

    Permutations

---
+++ Leetcode/Medium/46. Permutations.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/permutations/
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        do{
+            output.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
+        
+        return output;
+    }
+};
